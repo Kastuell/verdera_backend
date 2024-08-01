58e1ab06197d854e3efa53a05847b3921f748934
Kastuell <sholotun@mail.ru>
2024-08-01 19:52:28 +1000

    qwe

--- src/main.ts
+++ src/main.ts
@@ -4,23 +4,25 @@ import { AppModule } from './app.module';
 import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
-
-
   const PORT = process.env.PORT || 7000;
 
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api')
-  app.getHttpAdapter().getInstance().disable('x-powered-by')
+  app.setGlobalPrefix('api');
+  app.getHttpAdapter().getInstance().disable('x-powered-by');
   app.use(cookieParser());
-  app.enableShutdownHooks()
+  app.enableShutdownHooks();
   app.enableCors({
-    origin: ['http://localhost:3000', "https://www.verdera.ru"],
+    origin: [
+      "http://localhost:3000/",
+      "https://www.verdera.ru/",
+      "https://verdera.ru/",
+    ],
     credentials: true,
     exposedHeaders: 'set-cookie',
   });
 
-  const prismaService = app.get(PrismaService)
+  const prismaService = app.get(PrismaService);
 
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
